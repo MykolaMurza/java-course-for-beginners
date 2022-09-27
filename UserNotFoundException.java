6142607ae473a6fd568fd18f9ed43ee3e2b4c00f
MykolaMurza <murza.na00@gmail.com>
2022-09-27 14:58:34 +0300

    Lesson 28. Add exception class and use it in main method.

---
+++ UserNotFoundException.java
@@ -0,0 +1,9 @@
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super("User not found!");
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
